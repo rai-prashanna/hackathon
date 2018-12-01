cee78798115b5a3600075d2d7c60bb09b64aec2b
prashanna <prai93@outlook.com>
2018-12-01 23:12:40 +0100

    o.1 backend relaese

--- api.py
+++ api.py
@@ -9,25 +9,109 @@ app.secret_key = "mys3cr3tk3y"
 api = Api(app)
 
 
+def keyword_search(path, keywordlist, limit = 5):
+    json_data = json_dump_opener(path)
+    job_list = jobs_of_keyword(json_data, keywordlist)
+    employer_list = employer_aggregation(job_list)
+
+    return employer_sort(employer_list['Employers'], limit)
+
+def jobs_of_keyword(data, keywordlist):
+    JOBS = {}
+
+    for job in data:
+        desc = job['PLATSBESKRIVNING'].lower()
+        jobb_id = job['PLATSNUMMER']
+        job_employer = job['AG_NAMN']
+        job_posted = job['FORSTA_PUBLICERINGSDATUM']
+        job_title = job['PLATSRUBRIK']
+        job_yrke = job['YRKE_ID']
+        skill_found = True
+
+        for n, key in enumerate(keywordlist):
+            if key.lower() not in desc:
+                skill_found &= False
+            else:
+                skill_found &= True
+
+        if skill_found:
+            JOBS[jobb_id] = {
+                'DatePosted' : job_posted,
+                'Title': job_title,
+                'EmployerName': job_employer,
+                'YrkeId': job_yrke
+            }
+
+    return JOBS
+
+def employer_aggregation(job_list):
+    TOP_EMPLOYERS = {
+        'Employers' : {},
+        'LastPosted' : {}
+    }
+
+    for job in job_list:
+        employer = job_list[job]['EmployerName']
+        if employer in TOP_EMPLOYERS['Employers']:
+            TOP_EMPLOYERS['Employers'][employer] += 1
+            date = job_list[job]['DatePosted']
+            if date > TOP_EMPLOYERS['LastPosted'][employer]:
+                 TOP_EMPLOYERS['LastPosted'][employer] = date
+        else:
+            TOP_EMPLOYERS['Employers'][employer] = 1
+            date = job_list[job]['DatePosted']
+            TOP_EMPLOYERS['LastPosted'][employer] = date
+    return TOP_EMPLOYERS
+
+def employer_sort(mydict, limit = 5):
+    return {key: mydict[key] for key in sorted(mydict, key=mydict.get, reverse=True)[:limit]}
+
+def last_posted(mydict, employer):
+    return mydict[employer]
+
+
+def json_dump_opener(path):
+    with open(path) as f:
+        json_data = []
+        line = f.readline()
+        id = 0
+        while line:
+            json_data.append(json.loads(line))
+            line = f.readline()
+            id += 1
+        return json_data
+
 class Linkedin(Resource):
     def get(self,id,item):
         os.system("scrapeli --user "+id+" -o prai.json")
         with open('prai.json') as f:
             data = json.load(f)
             # print(data)
-            if int(item) ==0:
-                if 'experiences' in data:
-                    experiences=data['experiences']
-                    # print(experiences)
-                    if 'jobs' in experiences:
-                        jobs=experiences['jobs']
-                        # print(jobs)
-                        return jobs,201
-            if int(item)==1:
-                if 'skills' in data:
+            # if int(item) ==0:
+            #     if 'experiences' in data:
+            #         experiences=data['experiences']
+            #         # print(experiences)
+            #         if 'jobs' in experiences:
+            #             jobs=experiences['jobs']
+            #             # print(jobs)
+            #             return jobs,201
+            # if int(item)==1:
+            #     if 'skills' in data:
+            #         skills=data['skills']
+            #         # print(skills)
+            #         return skills,201
+            list_of_skills=[]
+            if 'skills' in data:
                     skills=data['skills']
-                    # print(skills)
-                    return skills,201
+                    for skill in skills:
+                        single_skill=skill['name']
+                        list_of_skills.append(single_skill)
+
+            result=keyword_search('test.json', list_of_skills[0])
+            return result,201
+
+
+
 
 
 
@@ -42,5 +126,6 @@ api.add_resource(Linkedin, '/extract/<id>/<item>')
 if __name__ == "__main__":
     app.debug = True
     app.run(host='0.0.0.0', port=8181)
+
 
 
--- test.py
+++ test.py
@@ -1,35 +1,105 @@
 import json
-from pprint import pprint
 
-# wholedata=""
-# with open('bmd007.json', 'r') as myfile:
-#     data=myfile.read().replace("\'", "\"")
-#     wholedata=wholedata+data
+def keyword_search(path, keywordlist, limit = 5):
+    json_data = json_dump_opener(path)
+    job_list = jobs_of_keyword(json_data, keywordlist)
+    employer_list = employer_aggregation(job_list)
+
+    return employer_sort(employer_list['Employers'], limit)
+
+def jobs_of_keyword(data, keywordlist):
+    JOBS = {}
+
+    for job in data:
+        desc = job['PLATSBESKRIVNING'].lower()
+        jobb_id = job['PLATSNUMMER']
+        job_employer = job['AG_NAMN']
+        job_posted = job['FORSTA_PUBLICERINGSDATUM']
+        job_title = job['PLATSRUBRIK']
+        job_yrke = job['YRKE_ID']
+        skill_found = True
+
+        for n, key in enumerate(keywordlist):
+            if key.lower() not in desc:
+                skill_found &= False
+            else:
+                skill_found &= True
+
+        if skill_found:
+            JOBS[jobb_id] = {
+                'DatePosted' : job_posted,
+                'Title': job_title,
+                'EmployerName': job_employer,
+                'YrkeId': job_yrke
+            }
+
+    return JOBS
+
+def employer_aggregation(job_list):
+    TOP_EMPLOYERS = {
+        'Employers' : {},
+        'LastPosted' : {}
+    }
+
+    for job in job_list:
+        employer = job_list[job]['EmployerName']
+        if employer in TOP_EMPLOYERS['Employers']:
+            TOP_EMPLOYERS['Employers'][employer] += 1
+            date = job_list[job]['DatePosted']
+            if date > TOP_EMPLOYERS['LastPosted'][employer]:
+                 TOP_EMPLOYERS['LastPosted'][employer] = date
+        else:
+            TOP_EMPLOYERS['Employers'][employer] = 1
+            date = job_list[job]['DatePosted']
+            TOP_EMPLOYERS['LastPosted'][employer] = date
+    return TOP_EMPLOYERS
+
+def employer_sort(mydict, limit = 5):
+    return {key: mydict[key] for key in sorted(mydict, key=mydict.get, reverse=True)[:limit]}
+
+def last_posted(mydict, employer):
+    return mydict[employer]
 
 
-import json
-from pprint import pprint
-import os
+def json_dump_opener(path):
+    with open(path) as f:
+        json_data = []
+        line = f.readline()
+        id = 0
+        while line:
+            json_data.append(json.loads(line))
+            line = f.readline()
+            id += 1
+        return json_data
 
-os.system("scrapeli --user bmd007 -o prai.json")
+skills=[
+    "Java",
+    "HTML",
+    "Android",
+    "Java Enterprise Edition",
+    "Android Development",
+    "Drupal",
+    "Hibernate",
+    "Spring Framework",
+    "CSS",
+    "Volleyball",
+    "Raspberry Pi",
+    "Object Oriented Design",
+    "JSF",
+    "JavaFX",
+    "Java Web Services",
+    "Primefaces",
+    "Apache Derby",
+    "Spring Boot",
+    "Groovy Grails",
+    "Spring Security",
+    "Microservices",
+    "CQRS",
+    "Groovy",
+    "Apache Kafka"
+]
 
-# with open('bmd007.json') as f:
-#     data = json.load(f)
-#
-# print("experiences are ::.............")
-# if 'experiences' in data:
-#     experiences=data['experiences']
-#     if 'jobs' in experiences:
-#         jobs=experiences['jobs']
-#         for item in jobs:
-#             print(item['date_range'])
-#             print(item['title'])
-#             print("*******")
-#
-# print("skills are ::.............")
-# if 'skills' in data:
-#     skills=data['skills']
-#     for item in skills:
-#             print(item['name'])
-#             print("*******")
-#
+print(keyword_search('test.json', skills[1]))
+
+
+
